58b63e0cd551d532e20b0d932dbfd4331dc6cb7f
patriciomackinlay <141454324+patriciomackinlay@users.noreply.github.com>
2023-11-08 13:28:11 -0300

    Complete game with console interface, begin GUI
    Game is now fully playable in the console, with game logic functioning
    as expected. Graphical UI for real game interactivity is on the way,
    with only board rendering working at the moment, but DOM implementation
    to the game should be easier with the program working correctly in the
    console.

--- script.js
+++ script.js
@@ -19,7 +19,35 @@ const gameboard = (function() {
         return boardArray;
     }
 
-    return {resetBoard, changeCell, getBoard}
+    function checkBoardForWin(marker) {
+        if(boardArray[0] === marker && boardArray[1] === marker && boardArray[2] === marker) {
+            return true;
+        } else if(boardArray[3] === marker && boardArray[4] === marker && boardArray[5] === marker) {
+            return true;
+        } else if(boardArray[6] === marker && boardArray[7] === marker && boardArray[8] === marker) {
+            return true;
+        } else if(boardArray[0] === marker && boardArray[4] === marker && boardArray[8] === marker) {
+            return true;
+        } else if(boardArray[1] === marker && boardArray[4] === marker && boardArray[7] === marker) {
+            return true;
+        } else if(boardArray[2] === marker && boardArray[4] === marker && boardArray[6] === marker) {
+            return true;
+        } else if(boardArray[0] === marker && boardArray[3] === marker && boardArray[6] === marker) {
+            return true;
+        } else if(boardArray[2] === marker && boardArray[5] === marker && boardArray[8] === marker) {
+            return true;
+        } 
+        return false;
+    }
+
+    function checkBoardForTie () {
+        if (boardArray.includes("")) {
+            return false;
+        }
+        return true;
+    }
+
+    return {resetBoard, changeCell, getBoard, checkBoardForWin, checkBoardForTie}
 })();
 
 function createPlayer (name, marker) {
@@ -29,24 +57,68 @@ function createPlayer (name, marker) {
 
 const displayController = (function () {
     const showCurrentBoard = () => console.log(gameboard.getBoard());
-    return {showCurrentBoard}
+
+    const renderBoard = () => {
+        const boardContainer = document.querySelector(".board-container");
+        const board = gameboard.getBoard();
+        board.forEach(element => {
+            const cell = document.createElement("div");
+            cell.classList.add("cell");
+            boardContainer.appendChild(cell);
+        });
+    }
+
+    
+
+    
+    return {showCurrentBoard, renderBoard}
 })();
 
 const gameFlow = (function() {
-    function playRound (gameboard, player) {
+    function playerTurn(player) {
         displayController.showCurrentBoard();
-        let playerSelection = prompt("Enter cell number!");
-        gameboard.changeCell(playerSelection, player.getMarker());
+        while (true) {
+            let playerSelection = prompt("Enter cell number!");
+            if (gameboard.getBoard()[playerSelection] !== "") {
+                alert("Cell is already taken!");
+                continue;
+            } else {
+                gameboard.changeCell(playerSelection, player.getMarker());
+                break;
+            }
+        }
     }
+    function playRound(player1, player2) {
+        displayController.renderBoard;
+        while(true) {
+            playerTurn(player1);
+            if (gameboard.checkBoardForWin(player1.getMarker())){
+                alert("Game Over! Player 1 wins!")
+                break;
+            }
+            if (gameboard.checkBoardForTie()) {
+                alert("Game Over! It's a tie!")
+                break;
+            }
+            playerTurn(player2);
+            if (gameboard.checkBoardForWin(player2.getMarker())){
+                alert("Game Over! Player 2 wins!")
+                break;
+            }
+            if (gameboard.checkBoardForTie()) {
+                alert("Game Over! It's a tie!")
+                break;
+            }
+        }
+    }
+
     return {playRound};
 })();
 
-function newGame (gameboard) {
+function newGame () {
     const player1 = createPlayer("Player 1", "X");
     const player2 = createPlayer("Player 2", "O");
-    for (let i = 0; i < 10; i++) {
-        gameFlow.playRound(gameboard, player1);
-    }
+    gameFlow.playRound(player1, player2);
 }
 
-newGame(gameboard);
+newGame();
